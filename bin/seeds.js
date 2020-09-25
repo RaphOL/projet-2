ab07dbbca9d35b981866c3b4ecbc7257489104d7
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-25 15:47:42 +0200

    sg commit

---
+++ bin/seeds.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const a = require("../models/model");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((self) => {
+    a.create(b)
+      .then((dbResult) => {
+        console.log(dbResult);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
