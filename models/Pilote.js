bbe940d4729228721a87e8e6b525215948c54011
Marcha <marchaf@gmail.com>
2020-09-28 11:55:14 +0200

    add pilote models and signin form

---
+++ models/Pilote.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const piloteSchema = new Schema({
+  name: String,
+  lastname: String,
+  email: String,
+  password: String,
+  licenceId: String,
+  issueDate: Date,
+  expiryDate: Date,
+  deliveryCountry: String,
+  licenceType: String,
+  ratingPilote: [String],
+  medicale: String,
+  flc055: String
+});
+
+const PiloteModel = mongoose.model("Pilote", piloteSchema);
+
+module.exports = PiloteModel;
