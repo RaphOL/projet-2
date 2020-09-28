4a9e59c46c2f820fd546f67c4a4ac23ad3e90e2f
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-28 13:03:27 +0200

    routes done

--- routes/authPilote.js
+++ routes/authPilote.js
@@ -2,18 +2,5 @@ const express = require("express");
 const router = new express.Router();
 const Pilote = require("../models/Pilote");
 const bcrypt = require("bcrypt");
-function test() {
-  console.log("pouet");
-}
-
-test();
-
-router.get("/signin/pilot", (req, res) => {
-  res.send("Pilote!!");
-});
-
-
 
 module.exports = router;
-
-
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,4 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/signin/user", function (req, res, next) {
-  res.render("signin/signinuser");
-});
-
-router.get("/signup/user", function (req, res, next) {
-  res.render("signup/signupuser");
-});
-
 module.exports = router;
