e8913d3758218747ab4c3e0fbe45ca07bf2229ae
Marcha <marchaf@gmail.com>
2020-09-28 16:00:58 +0200

    add flash messages

--- routes/authPilote.js
+++ routes/authPilote.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = new express.Router();
-const Pilote = require("../models/Pilote");
+const Pilot = require("../models/Pilote");
 const bcrypt = require("bcrypt");
-
-console.log("pouet");
 
 
 router.get("/signin/pilot", function (req, res, next) {
@@ -14,4 +12,27 @@ router.get("/signin/pilot", function (req, res, next) {
     res.render("signup/signuppilot");
   });
 
+  router.post("/signup/pilot", async (req, res, next) => {
+    const { email, name, lastname, password } = req.body;
+    try {
+        const foundPilot = await Pilot.findOne({email});
+        if (foundPilot) {
+          //  req.flash("error", "Email already taken");
+            req.session.msg = { status: 401, text: "Email already taken." };
+            return res.redirect("/signup/pilot");
+        }
+        // Add some salts just a little
+        const salt = 10;
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        await Pilot.create({
+            name,
+            email,
+            lastname,
+            password: hashedPassword,
+        });
+        res.redirect("/");
+     } catch (err) {
+       next(err);
+     }
+  });
 module.exports = router;
