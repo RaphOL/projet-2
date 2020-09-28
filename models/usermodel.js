6a8314aec441070f629b3c1683e391482078d26b
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-28 17:29:58 +0200

    fdifheif

--- models/usermodel.js
+++ models/usermodel.js
@@ -6,6 +6,11 @@ const userSchema = new Schema({
   lastname: String,
   email: String,
   password: String,
+  image: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhVCVV2vEZkPS1sMCHgmPsfLpWfr8wb-OubQ&usqp=CAU",
+  },
 });
 
 const User = mongoose.model("User", userSchema);
