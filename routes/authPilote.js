e2ec25d5646d1aa6c1fa75158806da1a03fa2165
Marcha <marchaf@gmail.com>
2020-09-28 16:37:23 +0200

    add element for pilote autenth

--- routes/authPilote.js
+++ routes/authPilote.js
@@ -7,7 +7,31 @@ const bcrypt = require("bcrypt");
 router.get("/signin/pilot", function (req, res, next) {
     res.render("signin/signinpilot");
   });
-  
+
+  router.post("/signin/pilot", async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const foundPilot = await Pilot.findOne({ email });
+      if (!foundPilot) {
+        // This will be handled, by the eraseSessionMessage middelware in app.js
+       // req.session.msg = { status: 401, text: "Invalid credentials" };
+        /**  Same could be done using the flash middleware **/ 
+         req.flash("error", "Invalid credentials");  // If you wanted to use flash you could aswell, you would have to handle i
+        return res.redirect("/signin/pilot");
+      }
+      if (!bcrypt.compareSync(password, foundPilot.password)) {
+         req.flash("error", "Invalid credentials");
+       // req.session.msg = { status: 401, text: "Invalid credentials" };
+        return res.redirect("/signin/pilot");
+      }
+      req.session.currentUser = foundPilot;
+      console.log("Pilot: ",req.session.currentUser);
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  });
+
   router.get("/signup/pilot", function (req, res, next) {
     res.render("signup/signuppilot");
   });
@@ -35,4 +59,5 @@ router.get("/signin/pilot", function (req, res, next) {
        next(err);
      }
   });
+
 module.exports = router;
