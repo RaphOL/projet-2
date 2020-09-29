f255270cbe49b634c8c789f3a85d967cebdda32d
Rico <rasim.rico@gmail.com>
2020-09-29 18:19:18 +0200

    searchFlight

--- routes/searchFlight.js
+++ routes/searchFlight.js
@@ -3,35 +3,20 @@ const express = require("express");
 const router = express.Router();
 const travelModel = require("../models/travelmodel");
 
-router.get("/search", async (req, res, netx) => {
-    try {
-      res.render("searchFlight.hbs");
-    } catch (err) {
-      next(err);
-    }
-  });
+router.get("/search", async (req, res, next) => {
+  try {
+    res.render("searchFlight");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/search", async (req, res, next) => {
+  try {
   
-  router.post("/search", async (req, res, next) => {
-    try {
-      const {
-        id_Pilote,
-        id_user,
-        numberOfSeats,
-        availableSeats,
-        immatriculation,
-        Date,
-        Price,
-        Departure,
-        Destination,
-        Aircraft,
-        departureTime,
-        arrvialTime,
-      } = req.body;
-      const newFlights = travelModel.find(req.body)
-      res.redirect(`/search/${req.session.currentUser._id, newFlights}`);
-    } catch (err) {
-      next(err);
-    }
-  });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
