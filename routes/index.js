7deb6c9434dc857666c9f1405d9dbde3b151a080
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-29 10:43:41 +0200

    jjjjj

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get("/about", function (req, res, next) {
   res.render("about");
 });
 
+router.get("/add", function (req, res, next) {
+  res.render("addFlight");
+});
+
+router.get("/search", function (req, res, next) {
+  res.render("searchFlight");
+});
+
 module.exports = router;
