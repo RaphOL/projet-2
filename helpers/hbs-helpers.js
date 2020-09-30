fd314440b149be0da5f48afdaff46bfb206d073c
Marcha <marchaf@gmail.com>
2020-09-30 12:33:15 +0200

    handling date in pilote

--- helpers/hbs-helpers.js
+++ helpers/hbs-helpers.js
@@ -1,7 +1,6 @@
 const hbs = require("hbs");
 const dayjs = require("dayjs");
 
-
 hbs.registerHelper("formatDateInput", function (date) {
   return dayjs(date).format("YYYY-MM-DDTHH:mm");
 });
@@ -9,3 +8,14 @@ hbs.registerHelper("formatDateInput", function (date) {
 hbs.registerHelper("formatDate", function (date) {
   return dayjs(date).format("YYYY-MM-DD HH:mm");
 });
+
+hbs.registerHelper("minDate", function(){
+  const today = new Date().now;
+  return dayjs(today).format("YYYY-MM-DDTHH:mm");
+});
+
+hbs.registerHelper("setDate", function(){
+  const today = new Date().now;
+  return dayjs(today).format("YYYY-MM-DDTHH:mm");
+});
+
