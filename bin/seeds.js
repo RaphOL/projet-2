a1d896a4c53a72b75129298d550a649a045733af
Rico <rasim.rico@gmail.com>
2020-10-02 10:43:18 +0200

    seedsmodidied

--- bin/seeds.js
+++ bin/seeds.js
@@ -4,19 +4,6 @@ const pilot = require("../models/Pilote");
 const user = require("../models/usermodel");
 const mongoose = require("mongoose");
 
-const travels = [
-  {
-    numberOfSeats: 4,
-    availableSeats: 3,
-    immatriculation: "F-ABVF",
-    Description: "",
-    image: "./images/Bordeaux-1.jpg",
-    Price: 67,
-    Departure: Bordeaux,
-    Destination: Nantes,
-    Aircraft: C182,
-  },
-];
 console.log(travels[0]);
 mongoose
   .connect(process.env.MONGO_URI, {
