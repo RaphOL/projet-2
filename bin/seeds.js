8bf9171e809585c67c51a9fac44d19325f200bcd
Rico <rasim.rico@gmail.com>
2020-10-02 10:41:53 +0200

    departure-image added,seeds ongoing

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
-const a = require("../models/model");
+const travel = require("../models/travelmodel");
+const pilot = require("../models/Pilote");
+const user = require("../models/usermodel");
 const mongoose = require("mongoose");
 
+const travels = [
+  {
+    numberOfSeats: 4,
+    availableSeats: 3,
+    immatriculation: "F-ABVF",
+    Description: "",
+    image: "./images/Bordeaux-1.jpg",
+    Price: 67,
+    Departure: Bordeaux,
+    Destination: Nantes,
+    Aircraft: C182,
+  },
+];
+console.log(travels[0]);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
